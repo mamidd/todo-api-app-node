2b96087ac92f99a35b327fe3c79f196a66bb4e81
Mario De Donno <mamidd@gmail.com>
2019-01-18 20:48:43 +0100

    add test for DELETE /users/me/token route

--- lesson95/server/tests/server.test.js
+++ lesson95/server/tests/server.test.js
@@ -248,7 +248,7 @@ describe('POST /users/login', () => {
       })
       .end((err, res) => {
         if(err){
-          done(err);
+          return done(err);
         }
 
         User.findById(users[1]._id).then((user) => {
@@ -274,7 +274,7 @@ describe('POST /users/login', () => {
       })
       .end((err, res) => {
         if(err){
-          done(err);
+          return done(err);
         }
 
         User.findById(users[1]._id).then((user) => {
@@ -284,3 +284,23 @@ describe('POST /users/login', () => {
       });
   });
 })
+
+describe('DELETE /users/me/token', () => {
+  it('should remove auth token on logout', (done) => {
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .send()
+      .expect(200)
+      .end((err, res) => {
+        if(err){
+          return done(err);
+        }
+
+        User.findById(users[0]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+});
